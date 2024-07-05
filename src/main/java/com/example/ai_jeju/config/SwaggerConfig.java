c3c1267932efe9726f2484b229d74a56390ccac0
femmefatalehaein <rmfls4359@gmail.com>
2024-07-05 23:58:35 +0900

    feat : config for swagger

---
+++ src/main/java/com/example/ai_jeju/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.ai_jeju.config;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+        public class SwaggerConfig {
+
+            @Bean
+            public OpenAPI openAPI() {
+                return new OpenAPI()
+                        .components(new Components())
+                        .info(apiInfo());
+            }
+
+            private Info apiInfo() {
+                return new Info()
+                        .title("Spring Boot REST API Specifications")
+                        .description("Specification")
+                        .version("1.0.0");
+            }
+        }
+
