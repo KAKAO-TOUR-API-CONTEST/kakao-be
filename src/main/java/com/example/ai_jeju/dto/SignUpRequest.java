49e79c83a3ad34e8140f11247853a2f1d3bb5d3e
femmefatalehaein <rmfls4359@gmail.com>
2024-07-18 20:21:00 +0900

    feat : child Model add

--- src/main/java/com/example/ai_jeju/dto/SignUpRequest.java
+++ src/main/java/com/example/ai_jeju/dto/SignUpRequest.java
@@ -3,6 +3,8 @@ package com.example.ai_jeju.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class SignUpRequest {
@@ -12,5 +14,7 @@ public class SignUpRequest {
     private String email;
     private String profile;
     private String provider;
+
+    private List<String> name;
 
 }
