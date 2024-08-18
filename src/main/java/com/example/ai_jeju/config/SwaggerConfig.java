4d91ae45760543865d0960ff97a543d2e820a8bc
femmefatalehaein <rmfls4359@gmail.com>
2024-08-18 21:28:11 +0900

    FEAT : updateUser API

--- src/main/java/com/example/ai_jeju/config/SwaggerConfig.java
+++ src/main/java/com/example/ai_jeju/config/SwaggerConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
         public class SwaggerConfig {
-
             @Bean
             public OpenAPI openAPI() {
                 return new OpenAPI()
