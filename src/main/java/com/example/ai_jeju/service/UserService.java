4c687e6b3409e51c6a2bf20a71818c2f7dc705a6
Yujin Lee <leeyujin1231@duksung.ac.kr>
2024-09-10 23:12:00 +0900

    fix : 원래대로 수정

--- src/main/java/com/example/ai_jeju/service/UserService.java
+++ src/main/java/com/example/ai_jeju/service/UserService.java
@@ -139,17 +139,10 @@ public class UserService {
         String access_token = tokenProvider.generateToken(newUser, ACCESS_TOKEN_DURATION);
         // DB에 refreshToken 저장
         saveRefreshToken(newUser.getId(), refresh_token);
-
-        Date now = new Date();
-        long accessTokenExpiresAt = now.getTime() + ACCESS_TOKEN_DURATION.toMillis();
         // AccessToken 쿠키로 발급
         addAccessTokenToCookie(request,response, access_token);
 
-        Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("access_token", access_token);
-        responseBody.put("expiresAt", accessTokenExpiresAt);
-
-        return ResponseUtil.SUCCESS("로그인 완료되었습니다.", responseBody);
+        return  ResponseUtil.SUCCESS("로그인 완료되었습니다.", access_token);
     }
 
     public User findByEmail(String email){
