bfecbda5a3b2de38af2cea6e4923efa702c68274
femmefatalehaein <rmfls4359@gmail.com>
2024-09-26 01:39:01 +0900

    [FIX] api/special 회원/비회원 조회 고려

--- src/main/java/com/example/ai_jeju/controller/SpecialRcmdController.java
+++ src/main/java/com/example/ai_jeju/controller/SpecialRcmdController.java
@@ -14,25 +14,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class SpecialRcmdController {
-
     private final TokenProvider tokenProvider;
     @Autowired
     private SpecialRcmdService specialRcmdService;
-
 
     public SpecialRcmdController(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
     }
 
-
     @GetMapping("/special")
-    ResponseDto getSpecialRcmd(@RequestParam int rcmdType, int category){
-        return ResponseUtil.SUCCESS("특별 추천 조회에 성공하였습니다", specialRcmdService.getSpecialRcmd(rcmdType,category));
+    ResponseDto getSpecialRcmd(@RequestHeader(value = "Authorization", required = false) String token,@RequestParam int rcmdType, int category) {
+        if (token != null) {
+            String accessToken = token.replace("Bearer ", "");
+            Long userId = tokenProvider.getUserId(accessToken);
+            if (tokenProvider.validToken(accessToken)) {
+
+                return ResponseUtil.SUCCESS("회원 특별 추천 조회에 성공하였습니다", specialRcmdService.getSpecialRcmd(rcmdType, category, userId));
+            } else {
+                return ResponseUtil.SUCCESS("유효하지 않은 사용자입니다.", null);
+            }
+        }
+            else{
+            return ResponseUtil.SUCCESS("비회원 특별 추천 조회에 성공하였습니다", specialRcmdService.getSpecialRcmd(rcmdType, category));
+            }
+        }
+
+
+
     }
-
-
-
-
-
-
-}
