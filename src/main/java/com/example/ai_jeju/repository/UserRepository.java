1de9afb9d12ee78c1d11c50c115f6e38081d6895
femmefatalehaein <rmfls4359@gmail.com>
2024-10-01 22:19:05 +0900

    [FIX]

--- src/main/java/com/example/ai_jeju/repository/UserRepository.java
+++ src/main/java/com/example/ai_jeju/repository/UserRepository.java
@@ -20,5 +20,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("UPDATE User u set u.valid = :valid WHERE u.id = :id")
     void updateUserByValid(@Param("id") Long userId, @Param("valid") Boolean valid);
 
+    @Modifying
+    @Query("UPDATE User u set u.valid = :valid, u.nickname = :nickname, u.profileImg = :profileImg," +
+            "u.provider = :provider , u.rgtDate = :rgtDate, u.phoneNum = :phoneNum," +
+            "u.ifRcmd = :ifRcmd, u.selectedCk1 = :selectedCk1, u.selectedCk2 = :selectedCk2 WHERE u.id = :id")
+    void updateUser(@Param("id") Long userId, @Param("valid") Boolean valid, @Param("nickname") String nickname,
+                    @Param("profileImg") String profileImg, @Param("provider") String provider,
+                    @Param("rgtDate") String rgtDate, @Param("phoneNum") String phoneNum,
+                    @Param("ifRcmd") Boolean ifRcmd,
+                    @Param("selectedCk1") Boolean selectedCk1, @Param("selectedCk2") Boolean selectedCk2);
+
+
 
 }
--- src/main/java/com/example/ai_jeju/service/UserService.java
+++ src/main/java/com/example/ai_jeju/service/UserService.java
@@ -163,16 +163,13 @@ public class UserService {
 
             return ResponseUtil.SUCCESS("로그인 완료되었습니다.", access_token);
         } else {
-
             // 한번 탈퇴한 회원일 경우에, 혹은 있거나?
             //이미 있는 회원일 때
-
             if(existingUser.get().isValid()){
                 return ResponseUtil.FAILURE("이미 가입된 회원입니다.", "");
             }else{
                 // 한번 탈퇴한 회원일 경우에
                 User user = existingUser.get();
-                userRepository.delete(user);
                 String nick = signUpRequest.getNickname();
                 if (nick == null) {
                     //일단 닉네임을 생성해보고.
@@ -185,29 +182,21 @@ public class UserService {
                 // 가입일자
                 LocalDate date = LocalDate.now();
                 // Save new user using builder pattern
-                User requestedUser = User.builder()
-                        .name(signUpRequest.getName())
-                        .nickname(nick)
-                        .provider(signUpRequest.getProvider())
-                        .email(signUpRequest.getEmail())
-                        .profileImg(signUpRequest.getProfileImg())
-                        .provider(signUpRequest.getProvider())
-                        .rgtDate(date.toString())
-                        .phoneNum(signUpRequest.getPhoneNum())
-                        .ifRcmd(signUpRequest.getIfRcmd())
-                        .valid(true)
-                        .build();
+                userRepository.updateUser(user.getId(), true, nick, signUpRequest.getProfileImg(),
+                        signUpRequest.getProvider(),
+                        date.toString(), signUpRequest.getPhoneNum(), signUpRequest.getIfRcmd(),
+                        signUpRequest.getSelectedCk1(), signUpRequest.getSelectedCk2());
                 /*-------------------------------------------*/
 
                 // 1. 부모정보 저장하기
-                User newUser = userRepository.save(requestedUser);
+                //User newUser = userRepository.save(requestedUser);
                 //Optional<User> registerdUser = userRepository.findByEmail(newUser.getEmail());
                 // 2. 동반아동 등록하기
                 List<ChildRequest> childList = signUpRequest.getChild();
                 for (int i = 0; i < childList.size(); i++) {
                     Child child = Child.builder()
                             // 유저 아이디의 값 그대로 주기.
-                            .user(newUser)
+                            .user(user)
                             .childName(childList.get(i).getChildName())
                             .birthDate(childList.get(i).getBirthDate())
                             .gender(childList.get(i).getGender())
@@ -217,20 +206,16 @@ public class UserService {
                     childRepository.save(child);
                 }
 
-                String refresh_token = tokenProvider.generateToken(newUser, REFRESH_TOKEN_DURATION);
-                String access_token = tokenProvider.generateToken(newUser, ACCESS_TOKEN_DURATION);
+                String refresh_token = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
+                String access_token = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
                 // DB에 refreshToken 저장
-                saveRefreshToken(newUser.getId(), refresh_token);
+                saveRefreshToken(user.getId(), refresh_token);
                 // AccessToken 쿠키로 발급
                 addAccessTokenToCookie(request, response, access_token);
 
                 return ResponseUtil.SUCCESS("로그인 완료되었습니다.", access_token);
-
             }
-
-
         }
-
 }
 
     public User findByEmail(String email){
